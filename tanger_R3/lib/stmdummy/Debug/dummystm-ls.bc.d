a0aeac0f0a87d80512f30547bfbe0a03c022f87e
Jinglei Ren <jinglei@persper.com>
2016-07-30 15:02:32 +0800

    Remove unnecessary files in tanger_R3

--- tanger_R3/lib/stmdummy/Debug/dummystm-ls.bc.d
+++
@@ -1,8 +0,0 @@
-/opt/tanger/37936/src/lib/stmdummy/Debug/dummystm-ls.ll \
-  /opt/tanger/37936/src/lib/stmdummy/Debug/dummystm-ls.bc.d \
-  /opt/tanger/37936/src/lib/stmdummy/Debug/dummystm-ls.ll: dummystm-ls.c \
-  dummystm.h /opt/tanger/37936/src/include/tanger-stm-internal.h
-
-dummystm.h:
-
-/opt/tanger/37936/src/include/tanger-stm-internal.h:
